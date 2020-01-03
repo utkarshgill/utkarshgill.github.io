e398c8120d026b0ae16073c4b67aabc3df39762c
qroach <ugill@ee.iitr.ac.in>
2020-01-03 05:37:46 +0530

    Updates

---
+++ precache-manifest.15161c720ee7091b9ad32fa6ed88f1de.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd022cf18a8ab098b04001c1986d71e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d346f058da5f722ad4d7",
+    "url": "/static/css/main.905b76af.chunk.css"
+  },
+  {
+    "revision": "3b0659fe4d61a9421c59",
+    "url": "/static/js/2.d1a37e29.chunk.js"
+  },
+  {
+    "revision": "5dd79dad86c6aed3b38eee6602719484",
+    "url": "/static/js/2.d1a37e29.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d346f058da5f722ad4d7",
+    "url": "/static/js/main.ee3cf11e.chunk.js"
+  },
+  {
+    "revision": "abe4d26ede49546260ca",
+    "url": "/static/js/runtime-main.b0577c27.js"
+  },
+  {
+    "revision": "62d4d7d369292a9bf23762465ec6d704",
+    "url": "/static/media/Proxima Nova Bold.62d4d7d3.otf"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/static/media/Proxima Nova Regular.410504d4.otf"
+  },
+  {
+    "revision": "6a386899746222073dd64c5f74d1a69d",
+    "url": "/static/media/Proxima Nova Semibold.6a386899.otf"
+  },
+  {
+    "revision": "2f735f2ac3e2871a13152c795c717ff4",
+    "url": "/static/media/arrow_upward-24px.2f735f2a.svg"
+  },
+  {
+    "revision": "8e40019819ede33e6ea52c14b49cf85c",
+    "url": "/static/media/behance.8e400198.svg"
+  },
+  {
+    "revision": "777b93c6d5a7b86081144f8d0d3ca1d8",
+    "url": "/static/media/close-24px.777b93c6.svg"
+  },
+  {
+    "revision": "76b80376f23d9f56314423d23695bbd1",
+    "url": "/static/media/dribbble.76b80376.svg"
+  },
+  {
+    "revision": "c8fac322fc9c382eceeb76066eb89839",
+    "url": "/static/media/facebook.c8fac322.svg"
+  },
+  {
+    "revision": "d150d4d60b034a7b8fc0df36dd389590",
+    "url": "/static/media/github-circle.d150d4d6.svg"
+  },
+  {
+    "revision": "f8486ce521e9e784d551780606a1341f",
+    "url": "/static/media/gmail.f8486ce5.svg"
+  },
+  {
+    "revision": "e5f5d20af21cf701bee9a33bbdb351e8",
+    "url": "/static/media/instagram.e5f5d20a.svg"
+  },
+  {
+    "revision": "c3d75740c5c58e55c4e6e9bac7e742ac",
+    "url": "/static/media/keyboard_arrow_down-24px.c3d75740.svg"
+  },
+  {
+    "revision": "f71c6d048d8365d41eb4f57f715e802c",
+    "url": "/static/media/linkedin-box.f71c6d04.svg"
+  },
+  {
+    "revision": "c275e5189ecaabdbfdc5407dc031c28b",
+    "url": "/static/media/menu-24px.c275e518.svg"
+  },
+  {
+    "revision": "b0228fdb96997140418f12753c636b5f",
+    "url": "/static/media/open-in-new.b0228fdb.svg"
+  },
+  {
+    "revision": "c73d19af214c9f8e15d14e719d2416e4",
+    "url": "/static/media/phone.c73d19af.svg"
+  },
+  {
+    "revision": "0b15b1c0224c4536c14098d8be1634a2",
+    "url": "/static/media/test-ls.0b15b1c0.jpg"
+  },
+  {
+    "revision": "6c4c82197fcd9cd54ddd46e20525ae06",
+    "url": "/static/media/test.6c4c8219.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "qroach.github.io/precache-manifest.f6116c90ac6f4bc283fbf6f7ab1188dc.js"
+  "/precache-manifest.15161c720ee7091b9ad32fa6ed88f1de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("qroach.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
