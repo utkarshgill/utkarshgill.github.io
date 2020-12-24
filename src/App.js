a682aa2da5a0602b2cba1214c6112de507600579
utkarshgill <ugill@ee.iitr.ac.in>
2020-12-24 17:53:35 +0530

    v4babyy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { useState } from 'react'
-import mypic from './assets/guy.jpg'
+import React from 'react'
 import './App.scss'
 
 import UseAnimations from 'react-useanimations';
 import download from 'react-useanimations/lib/download'
 import linkedin from 'react-useanimations/lib/linkedin'
+import twitter from 'react-useanimations/lib/twitter'
+
+import arrowDown from 'react-useanimations/lib/arrowDown'
+import arrowRight from 'react-useanimations/lib/arrowRightCircle'
 import mail from 'react-useanimations/lib/mail'
 
-import { Frame, Page, Scroll, Stack } from "framer"
+import cover from "./assets/cover.jpg"
+import unicorn from "./assets/unicorn.jpg"
+import black from "./assets/black-bg.jpg"
+import resume from "./assets/Resume-UtkarshGill.pdf"
+
+import { Frame, Page, Stack } from "framer"
 import { projects } from './project_data'
-import resume from './assets/guy.jpg'
+
+
+
 
 function App() {
   const openInNewTab = (url) => {
@@ -17,94 +27,226 @@ function App() {
     if (newWindow) newWindow.opener = null
   }
 
-  const main = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        duration: 1.2
-      }
+  var responsive,
+    align,
+    dist,
+    contentWidth,
+    contentHeight,
+    marginSide,
+    marginTop,
+    marginBottom,
+    conDist,
+    workDist,
+    projectMargin,
+    projectGap,
+    projectWidth,
+    scrollable,
+    iconWidth,
+    imgHeight,
+    imgWidth,
+    conGap
+
+  function myFunction(x) {
+    if (x.matches) { // If media query matches
+      responsive = "vertical"
+      align = "start"
+      dist = "space-between"
+      contentHeight = "auto"
+      contentWidth = "100%"
+      marginSide = 20
+      marginTop = 48
+      marginBottom = 60
+      conDist = "start"
+      workDist = "center"
+      projectMargin = 20
+      projectGap = 0
+      projectWidth = "80vw"
+      scrollable = "false"
+      iconWidth = "100%"
+      imgWidth = "150px"
+      imgHeight = "200px"
+      conGap = 24
+
+    } else {
+      responsive = "horizontal"
+      align = "center"
+      dist = "space-evenly"
+      contentHeight = "400px"
+      contentWidth = "600px"
+      marginSide = 0
+      marginTop = 0
+      marginBottom = 0
+      conDist = "space-between"
+      workDist = "center"
+      projectMargin = window.innerWidth / 8
+      projectGap = window.innerWidth / 20
+      projectWidth = "360px"
+      scrollable = "true"
+      iconWidth = "248px"
+      imgHeight = "640px"
+      imgWidth = "480px"
+      conGap = 60
     }
   }
 
-  const item = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        delay: 2.4 ,
-        duration: 0.6
-      }
-    }
-  }
+  var x = window.matchMedia("(max-width: 700px)")
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction);
 
   return (
-    <Stack
-    initial="hidden"
-    animate="show" gap={0} center="x" width="100%" style={{maxWidth: "1200px"}}>
+    <Page
+      gap={0}
+      width="100vw"
+      height={window.innerHeight}
+      wheelEnabled={scrollable}
+      direction="vertical"
+      defaultEffect="cube"
+      directionLock={true}
+      contentHeight="auto"
+    >
       <Stack
-        center="x"
-        backgroundColor="rgba(27, 27, 27, 0.95)"
-        top={0}
-        gap={0}
-        style={{ zIndex: "10", boxSizing: "border-box" }}
-        width="100%" height="auto"
-        direction="horizontal"
-        distribution="space-between"
-        alignment="end"
-        paddingTop={40}
-        paddingLeft={24}
-        paddingRight={24}
-        variants={item}
-      >
-        <span className="text">gill.design</span>
-        <Stack direction="horizontal" width="auto" height="auto" gap={32}>
-          <a href="mailto: utkarshgill07@gmail.com"><UseAnimations size={28} strokeColor="#fff" animation={mail} /></a>
-          <a href="https://www.linkedin.com/in/utkarsh-gill-9a5029173/"><UseAnimations size={28} strokeColor="#fff" animation={linkedin} /></a>
-          <a href={resume} download><UseAnimations size={28} strokeColor="#fff" animation={download} /></a>
-        </Stack>
-      </Stack>
-
-
-      <Stack
-        center="x"
-        gap={0}
+        gap={32}
         width="100%"
-        height="auto"
-        style={{
-          boxSizing: "border-box",
-        }}
-        paddingRight={24}
-        paddingLeft={24}
+        height={window.innerHeight}
+        direction={responsive}
+        alignment="center"
+        distribution={dist}
+        backgroundColor="white"
+
+        style={{ boxSizing: "border-box" }}
+        paddingLeft={marginSide}
+        paddingRight={marginSide}
+        paddingTop={marginTop}
+        paddingBottom={marginBottom}
+      >
+        <div className="scroll-text-box">
+          <UseAnimations strokeColor="white" size="32" animation={arrowDown} />
+          <p className="scroll-text">Scroll to view my work</p>
+        </div>
+        <Stack
+          gap={conGap}
+          direction="vertical"
+          distribution="center"
+          height="auto"
+          width={contentWidth}
+          alignment="start"
+          style={{ boxSizing: "border-box" }}
+        >
+          <p className="head"><span>Hello, I’m Gill. </span><br />I design the obvious.</p>
+          <p className="info">Product design enthusiast and a dank unicorn. Upcoming design intern at <a
+            rel="noopener,noreferrer"
+            target="_blank"
+            href="https://www.nutanix.com"
+          >Nutanix</a>, previously at <a
+            rel="noopener,noreferrer"
+            target="_blank"
+            href="https://www.postman.com"
+          >Postman</a>.</p>
+          <Stack
+            gap={20}
+            direction="vertical"
+            height="auto"
+            width={iconWidth}
+          >
+            <Frame width="100%" height="2px" backgroundColor="black" />
+            <Stack
+              direction="horizontal"
+              distribution="space-between"
+              width="100%"
+              height="auto"
+            >
+              <a style={{}} href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=utkarshgill07@gmail.com" target="_blank" rel="noopener,noreferrer" >
+                <UseAnimations strokeColor="black" size="32" animation={mail} />
+              </a>
+              <a style={{}} href="https://twitter.com/UtkarshGill5" target="_blank" rel="noopener,noreferrer" >
+                <UseAnimations strokeColor="black" size="32" animation={twitter} />
+              </a>
+              <a style={{}} href="https://www.linkedin.com/in/utkarsh-gill-9a5029173/" target="_blank" rel="noopener,noreferrer" >
+                <UseAnimations strokeColor="black" size="32" animation={linkedin} />
+              </a>
+              <a style={{}} download href={resume} target="_blank" rel="noopener,noreferrer" >
+                <UseAnimations strokeColor="black" size="32" animation={download} />
+              </a>
+            </Stack>
+          </Stack>
+
+        </Stack>
+        <Frame width={imgWidth} height={imgHeight} image={unicorn} />
+      </Stack>
+      <Stack
+        id="work"
+        gap={16}
+        width="100%"
+        height={window.innerHeight}
+        backgroundColor="black"
+        distribution={workDist}
+        alignment="start"
+        style={{ boxSizing: "border-box" }}
       >
 
-        <Stack variants={main} gap={16} distribution="center" alignment="center" height="64vh" width="100%" >
-          <p className="title">Hi, I'm Gill.
-          <span > I design the obvious.</span>
-          </p>
-          <Stack variants={item} gap={0} width="auto" height="auto"><p variants={item} className="sub-text">Upcoming Product Design Intern at Nutanix. Summer 2020 Intern at Postman.</p>
-        </Stack>
-        </Stack>
-
-
-        <Stack variants={item} gap={16} alignment="start" width="100%" height="auto">
-          <span className="text">My work</span>
-          <span className="sub-text">A directory of my projects and work experiences</span>
-        </Stack>
-        <Stack variants={item} gap={16} width="100%" height="auto" paddingTop={48} paddingBottom={60}>
+        <p className="head2">Work</p>
+        <Page
+          width="100%"
+          height="75%"
+          contentWidth="auto"
+          contentHeight="auto"
+          wheelEnabled={scrollable}
+          defaultEffect="wheel"
+          direction="horizontal"
+          alignment="center"
+          paddingLeft={projectMargin}
+          paddingRight={projectMargin}
+          gap={projectGap}
+        >
           {projects.map((elem, index) => {
-            return <Frame image={elem.banner} whileHover={{ width: "101%" }} style={{ pointerEvents: "all" }} onClick={() => { openInNewTab(elem.link) }} backgroundColor="#6173FF" position="relative" width="100%" height="360px" radius="14px">
-              <Stack gap={8} style={{ maxWidth: "100%", boxSizing: "border-box" }} alignment="start" width="100%" height="auto" bottom={0} paddingBottom={28} paddingTop={28} paddingLeft={24} paddingRight={24}>
-                <span className="sub-text">{elem.subtext}</span>
-                <span className="text project">{elem.title}</span>
+            return <Frame
+              width={projectWidth}
+              height="100%"
+              backgroundColor={elem.color}
+              borderRadius={1}
+            >
+              <Stack
+                distribution="space-between"
+                alignment="start"
+                padding="24"
+                paddingTop="48"
+                paddingBottom="48"
+                width="100%"
+                height="100%"
+                style={{ boxSizing: "border-box" }}
+              >
+
+
+                <Stack
+                  gap={16}
+                  alignment="start"
+                  width="100%"
+                  height="auto"
+
+                >
+                  <p className="sub-head">{elem.subtext}</p>
+                  <p className="head3">{elem.title}</p>
+                </Stack>
+                <a style={{width: "100%",  height: "32px"}} href={elem.link} rel="noopener,noreferrer" target="_blank">
+                <Stack
+                    direction="horizontal"
+                    width="100%"
+                    height="auto"
+                    alignment="center"
+                  >
+                    <UseAnimations strokeColor="white" size="32" animation={arrowRight} />
+                    <p className="scroll-text">Read case study</p>
+                  </Stack>
+                </a>
+
               </Stack>
+
+
             </Frame>
           })}
-        </Stack>
-        <Stack width="100%"><span className="sub-text">I designed and developed this website from scratch © 2020</span></Stack>
+        </Page>
       </Stack>
-    </Stack>
-
+    </Page>
   )
 }
 
--- src/project_data.js
+++ src/project_data.js
@@ -1,34 +1,44 @@
 export const projects = [
-{ 
+    { 
+        title: "Designing for Collaboration",
+        subtext: "My internship with Postman",
+        link: "https://utkarsh-gill.medium.com/ship-learn-and-iterate-dont-overthink-f8ad1e7175f7",
+        color: "#f26b3a"
+    },
+
+    { 
         title: "How I Designed a Social Credit System",
         subtext: "Nutanix Design Competition 2020 Finalist",
-        banner: require("./assets/nutanix2.jpg"),
         link: "https://medium.com/usabilitygeek/240-hours-of-ux-nutanix-design-competition-f3a98123ccdb",
+        color: "#e83c64"
+    },
+
+    {    
+        title: "Redesigning a Logistics Management Platform",
+        subtext: "My internship with Wheelseye",
+        link: "https://medium.com/design-studio-iit-roorkee/designing-for-the-employees-winter-2019-internship-at-wheelseye-8b9c15705670",
+        color: "#2c7db4"
     },
 
     { 
         title: "Optimising the Hiring Process for Recruiters",
         subtext: "Chrome extension for Gmail",
-        banner: require("./assets/pink.jpg"),
         link: "https://www.behance.net/gallery/97758399/Hiring-Console",
+        color: "#57c9fb"
     },
 
-    {    
-        title: "My Internship with Wheelseye",
-        subtext: "Ground logistics management tool",
-        banner: require("./assets/blue.jpg"),
-        link: "https://medium.com/design-studio-iit-roorkee/designing-for-the-employees-winter-2019-internship-at-wheelseye-8b9c15705670",
-    },
     { 
-        title: "Reviewing the Collaborative Experience of Google Docs",
-        subtext: "Heuristics evaluation of Google Docs",
-        banner: require("./assets/orange.jpg"),
+        title: "Reviewing the Collaboration Experience of Google Docs",
+        subtext: "Heuristics evaluation",
         link: "https://medium.com/@utkarshgill07/collaboration-over-google-docs-a-not-so-good-experience-5c3caca86cdc?sk=55b18fad41c7297f348087911c4d6c84",
+        color: "#4c8bf5"
     },
+
     { 
-        title: "My First UX Project - Kookerr",
-        subtext: "Voice assisted cooking",
-        banner: require("./assets/grey.jpg"),
+        title: "Voice-assisted Cooking Experience",
+        subtext: "My first UX design project",
         link: "https://medium.com/design-studio-iit-roorkee/a-hands-free-cooking-assistant-daa2fec255d",
+        color: "#febe0e"
     },
+    
 ]
